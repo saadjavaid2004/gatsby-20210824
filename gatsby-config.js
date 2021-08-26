9c7519493eb908f4c38c3f1c0d2ee7ea8c703dc7
saadjavaid2004 <saadjavaid2004@gmail.com>
2021-08-26 19:55:53 +0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+	{
+		resolve: "gatsby-plugin-manifest",
+		options: {
+			name: "PWA Gatsby",
+			short_name: "PWA Gatsby",
+			start_url: "/",
+			background_color: "#f7f0eb",
+			theme_color: "#a2466c",
+			display: "standalone",
+			icon: "src/images/icon.png",
+		},
+	},
+	"gatsby-plugin-offline",
   ],
 };

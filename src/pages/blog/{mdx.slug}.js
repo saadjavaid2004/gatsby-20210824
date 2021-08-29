93fa7b084b1719ce16538a41d03d15d4c7f4f129
saadjavaid2004 <saadjavaid2004@gmail.com>
2021-08-29 18:35:23 +0500

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,30 +1,43 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx' // highlight-line
+import { GatsbyImage, getImage } from 'gatsby-plugin-image' // highlight-line
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => { // highlight-line
+	const image = getImage(data.mdx.frontmatter.hero_image) // highlight-line
+	
   return (
     
     <Layout pageTitle={data.mdx.frontmatter.title}>
-	{/* highlight-start */}
       <p>{data.mdx.frontmatter.date}</p>
+	  <GatsbyImage
+      image={image}
+      alt={data.mdx.frontmatter.hero_image_alt}
+    />
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
-    {/* highlight-end */}
     </Layout>
   )
 }
 
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: {eq: $id}) {
+  query($slug: String) {
+    mdx(slug: {eq: $slug}) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `
